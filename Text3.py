2373422693fc3f2b22498acff8e77e8260d5e552
Rahul Khanna <sociallyRK@users.noreply.github.com>
2024-11-24 22:52:16 -0800

    Update Text3.py

--- Text3.py
+++ Text3.py
@@ -2,7 +2,7 @@ from twilio.rest import Client
 
 # Twilio credentials
 ACCOUNT_SID = "AC30cd08d8f54f6bd71e82510d3de45a72"
-AUTH_TOKEN = "02fa887cab4009b3957d8f679923bc10"
+AUTH_TOKEN = ""
 
 # Twilio client
 client = Client(ACCOUNT_SID, AUTH_TOKEN)
